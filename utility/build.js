5bf5566c9feec1b3300a010e2381a0a6027066bf
Moritz Habenbacher <89516674+dgatialy@users.noreply.github.com>
2022-01-23 17:48:06 +0100

    Create tokens

---
+++ utility/build.js
@@ -0,0 +1,100 @@
+const StyleDictionaryPackage = require('style-dictionary')
+const ptr = (px) => {
+    return 1/16 * parseFloat(px)
+}
+// HAVE THE STYLE DICTIONARY CONFIG DYNAMICALLY GENERATED
+
+StyleDictionaryPackage.registerFormat({
+  name: 'css/variables',
+  formatter: function (dictionary, config) {      
+    return `${this.selector} {
+        ${dictionary.allProperties
+          .map((prop) => `  --${dictionary.options.theme}-${prop.name}: ${prop.value};`)
+          .join('\n')}
+      }`
+  },
+})
+
+StyleDictionaryPackage.registerTransform({
+  name: 'sizes/rem',
+  type: 'value',
+  matcher: function (prop) {        
+    return [      
+      'spacing',      
+      'borderRadius',
+      'borderWidth',
+      'sizing',
+      'padding'
+    ].includes(prop.original.type) || [            
+        'fontSize',
+        'paragraphSpacing',
+        'lineHeight'
+      ].includes(prop.attributes.type)
+  },
+  transformer: function (prop) {        
+    if (isNaN(parseFloat(prop.original.value))) return prop.original.value;
+
+    return ptr(prop.original.value) + 'rem';        
+  },
+})
+
+StyleDictionaryPackage.registerTransform({
+    name: 'font/weight',
+    type: 'value',
+    matcher: function (prop) {
+        return [            
+            'fontWeight',
+          ].includes(prop.attributes.type)
+    },
+    transformer: function (prop) {
+        console.log(prop)
+        const mapping = { 
+            'SemiBold' : 600,
+            'Medium': 500            
+        }
+        
+        return mapping[prop.original.value] ? mapping[prop.original.value] : prop.original.value
+    },
+  })
+
+function getStyleDictionaryConfig(theme) {
+  return {
+    source: [`tokens/${theme}.json`],
+    platforms: {
+      web: {
+        transforms: ['attribute/cti', 'name/cti/kebab', 'sizes/rem', 'font/weight'],
+        buildPath: `output/`,
+        files: [
+          {
+            destination: `${theme}.css`,
+            format: 'css/variables',
+            selector: `:root`,
+            options: {
+                theme: theme
+            }
+          },
+        ],
+      },
+    },
+  }
+}
+
+console.log('Build started...')
+
+// PROCESS THE DESIGN TOKENS FOR THE DIFFEREN BRANDS AND PLATFORMS
+
+;['card'].map(function (theme) {
+  console.log('\n==============================================')
+  console.log(`\nProcessing: [${theme}]`)
+
+  const StyleDictionary = StyleDictionaryPackage.extend(
+    getStyleDictionaryConfig(theme)
+  )
+
+  StyleDictionary.buildPlatform('web')
+
+  console.log('\nEnd processing')
+})
+
+console.log('\n==============================================')
+console.log('\nBuild completed!')
